b5bdbb1988478a5918eacb25986e6b71e850d32f
Marcello Silvério <100056128+smarcelloc@users.noreply.github.com>
2022-04-28 21:46:45 -0300

    add: router annotation

--- src/Controller/MammalController.php
+++ src/Controller/MammalController.php
@@ -3,11 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class MammalController
 {
+  /**
+   * @Route("/")
+   * 
+   * @return Response
+   */
   public function index()
   {
-    return new Response('Hello World !!!', Response::HTTP_OK);
+    return new Response('Zoo !!!', Response::HTTP_OK);
+  }
+
+  /**
+   * @Route("/show/{animal}")
+   *
+   * @param string $animal
+   * @return void
+   */
+  public function show(string $animal)
+  {
+    $animal = mb_ereg_replace('-', ' ', $animal);
+    $content = sprintf('Mammal detail page "%s"', $animal);
+
+    return new Response($content, Response::HTTP_OK);
   }
 }

83a641239b6003fbb0ecb0cc067a5d5b8943a5d2
Marcello Silvério <100056128+smarcelloc@users.noreply.github.com>
2022-04-30 18:52:30 -0300

    add: using template PHP - Twig

--- src/Controller/MammalController.php
+++ src/Controller/MammalController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MammalController
+class MammalController extends AbstractController
 {
   /**
    * @Route("/")
@@ -29,5 +30,34 @@ class MammalController
     $content = sprintf('Mammal detail page "%s"', $animal);
 
     return new Response($content, Response::HTTP_OK);
+  }
+
+  /**
+   * @Route("/list/{animal}")
+   *
+   * @param string $animal
+   * @return string
+   */
+  public function list(string $animal)
+  {
+    $persons = [
+      [
+        "name" => "John Joy",
+        "cpf" => "111.111.111-11",
+        "genro" => "man"
+      ],
+      [
+        "name" => "Julia Mendes",
+        "cpf" => "222.222.222-22",
+        "genro" => "women"
+      ],
+      [
+        "name" => "Kaio Pereira",
+        "cpf" => "333.333.333-33",
+        "genro" => "man"
+      ]
+    ];
+
+    return $this->render('animal/list.twig', compact('animal', 'persons'));
   }
 }

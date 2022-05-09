6a9a7c5e091c59ee93032fd364fd8fcdc1074888
Marcello Silvério <100056128+smarcelloc@users.noreply.github.com>
2022-05-08 21:22:36 -0300

    API Endpoint

---
+++ src/Controller/Api/NumberRandController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NumberRandController extends AbstractController
+{
+
+  /**
+   * @Route("/api/rand/{min}/{max}")
+   */
+  public function index(string $min, string $max)
+  {
+    if (!filter_var($min, FILTER_VALIDATE_INT) || !filter_var($max, FILTER_VALIDATE_INT)) {
+      return $this->json(['status' => false, 'message' => 'parameter invalid'], Response::HTTP_BAD_REQUEST);
+    }
+
+    $number = rand($min, $max);
+    return $this->json(['status' => true, 'number' => $number]);
+  }
+}
